2538da8835afe4918e378e8be16be6d98a33ee66
Brandon Booth <bbooth@usc.edu>
2015-05-17 18:46:50 -0700

    Fixed library import so script runs from any working dir
    Former-commit-id: ec216a96a3775d245338bb296debfa95c1542d92

--- ros/src/player_eeg/scripts/player_eeg_pub.py
+++ ros/src/player_eeg/scripts/player_eeg_pub.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-
 import sys,os
 import time
 import ctypes
+import ntpath
 from ctypes import *
 import rospy
 from player_eeg.msg import EEGData
 
 #--------------------------------------------------------------------------
 
-srcDir = os.getcwd()
-libPath = srcDir + "/src/player_eeg/scripts/libedk.so.1.0.0" 
+srcDir = os.path.realpath(__file__)
+headPath, tailPath = ntpath.split(srcDir)
+srcDir = headPath or ntpath.basename(headPath)
+libPath = srcDir + "/../third_party/emotiv_epoc/lib/libedk.so.1.0.0"
 libEDK = CDLL(libPath)
 
 #--------------------------------------------------------------------------
@@ -159,8 +160,8 @@ print "Start receiving EEG Data! Press any key to stop logging...\n"
 hData = libEDK.EE_DataCreate()
 libEDK.EE_DataSetBufferSizeInSec(secs)
 
-fake_eeg_pub = rospy.Publisher('eeg', EEGData, queue_size=1)
-rospy.init_node('EEG-Node', anonymous=True)	
+fake_eeg_pub = rospy.Publisher('player_eeg', EEGData, queue_size=1)
+rospy.init_node('player_eeg', anonymous=True)	
 rate = rospy.Rate(15)
 while not rospy.is_shutdown():
     state = libEDK.EE_EngineGetNextEvent(eEvent)
