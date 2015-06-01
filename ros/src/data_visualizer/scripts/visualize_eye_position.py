d0a5bbd3d24df45a08091fdc91de4b8a5ab44b3b
Brandon Booth <bbooth@usc.edu>
2015-05-31 18:32:59 -0700

    Overlay eye position on game video
    This runs too slow due to the list() conversion of immutable string data
    
    
    Former-commit-id: 5b1e83f026054bd9be2182b0eff96715dd8c3eb3

--- ros/src/data_visualizer/scripts/visualize_eye_position.py
+++ ros/src/data_visualizer/scripts/visualize_eye_position.py
@@ -4,36 +4,61 @@ import rospy
 from sensor_msgs.msg import Image
 from player_eye_tracking.msg import Position2D
 
-eyePosImage = Image()
+gameVideoImageData = None
 eyePosImagePub = None
-eyePosImageWidth = 480
-eyePosImageHeight = 270
+eyePosImage = Image()
+eyePosImageWidth = 480 # Only used if no game video images are being published
+eyePosImageHeight = 270 # Only used if no game video images are being published
+temp = 1
+
+def gameVideoCallback(gameVideoData):
+   global eyePosImagePub
+   global gameVideoImageData
+   global temp
+
+   eyePosImage.width = gameVideoData.width 
+   eyePosImage.height = gameVideoData.height
+   eyePosImage.step = gameVideoData.step
+   eyePosImage.encoding = gameVideoData.encoding
+   eyePosImage.header.frame_id = gameVideoData.header.frame_id
+   gameVideoImageData = list(gameVideoData.data);
 
 def eyePositionCallback(eyePositionData):
    global eyePosImagePub
+   global gameVideoImageData
 
    eyePosImage.header.stamp = rospy.get_rostime()
+   if (gameVideoImageData is not None):
+      eyePosImage.data = gameVideoImageData
 
    # Put a dot in the image where the eye position lies
    centerX = int(eyePositionData.posX*eyePosImage.width)
    centerY = int(eyePositionData.posY*eyePosImage.height)
 
-   # Add a black spot at the eye position
+   # Add a spot at the eye position
    radius = 5
+   valuesPerPixel = eyePosImage.step/eyePosImage.width
    for x in range(centerX-radius, centerX+radius+1):
       for y in range(centerY-radius, centerY+radius+1):
          if x > 0 and x < eyePosImage.width and y > 0 and y < eyePosImage.height:
             pixelIndex = x + y*eyePosImage.width
-            eyePosImage.data[3*pixelIndex:3*pixelIndex + 3] = 3*[0]
+            replacementList = valuesPerPixel*[0]
+            if (eyePosImage.encoding != 'bgr8'):
+               replacementList = valuesPerPixel*['\x00']
+            eyePosImage.data[valuesPerPixel*pixelIndex:valuesPerPixel*pixelIndex + valuesPerPixel] = replacementList
+
+   if (gameVideoImageData is not None):
+      eyePosImage.data = "".join(eyePosImage.data)
 
    eyePosImagePub.publish(eyePosImage)
 
-   # Return the image to solid color fill
-   for x in range(centerX-radius, centerX+radius+1):
-      for y in range(centerY-radius, centerY+radius+1):
-         if x > 0 and x < eyePosImage.width and y > 0 and y < eyePosImage.height:
-            pixelIndex = x + y*eyePosImage.width
-            eyePosImage.data[3*pixelIndex:3*pixelIndex + 3] = 3*[128]
+   if (gameVideoImageData is None):
+      # Return the image to solid color fill
+      for x in range(centerX-radius, centerX+radius+1):
+         for y in range(centerY-radius, centerY+radius+1):
+            if x > 0 and x < eyePosImage.width and y > 0 and y < eyePosImage.height:
+               pixelIndex = x + y*eyePosImage.width
+               eyePosImage.data[valuesPerPixel*pixelIndex:valuesPerPixel*pixelIndex + valuesPerPixel] = valuesPerPixel*[128]
    
 
 def generateEyePosImage():
@@ -46,6 +71,7 @@ def generateEyePosImage():
    eyePosImage.data = [128 for _ in range(eyePosImage.height*eyePosImage.step)]
 
    rospy.init_node('visualizerEyePosition', anonymous=False)
+   gameVideoSub = rospy.Subscriber('/gameVideo', Image, gameVideoCallback)
    eyePosSub = rospy.Subscriber('/eye_position', Position2D, eyePositionCallback)
    eyePosImagePub = rospy.Publisher('eye_position_image', Image, queue_size=1)
    rospy.spin()
