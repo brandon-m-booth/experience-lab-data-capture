a5d32a50a385e6d0acf219b5e97590e5fda5497f
Brandon Booth <bbooth@usc.edu>
2016-05-19 17:44:13 -0700

    Added rqt_ez_publisher to package install script
    Former-commit-id: dd998512ba6f30c411815fb37597e3cf8caf08b5

--- install_packages.sh
+++ install_packages.sh
@@ -7,3 +7,11 @@ sudo apt-get install ros-indigo-freenect-stack -y
 
 # Audio dependencies
 sudo apt-get install libmp3lame-dev
+
+# RQT dependencies
+pushd /opt/ros/indigo/share
+sudo git clone https://github.com/OTL/rqt_ez_publisher.git
+cd rqt_ez_publisher
+sudo python setup.py install
+rm ~/.config/ros.org/rqt_gui.ini
+popd
