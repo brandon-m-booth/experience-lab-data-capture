cf0a1c53d81d8b04fecb431258a02e2048a3b84f
bbooth <brandon.m.booth@gmail.com>
2016-05-24 19:10:53 -0700

    Add ros package repo url during setup install
    Former-commit-id: 4a7e16f890d61ee96e72ed4aa1c075330b3743d6

--- install_packages.sh
+++ install_packages.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ ! -f /etc/apt/sources.list.d/ros-latest.list ]; then
+	sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+	sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
+	sudo apt-get update
+fi
+
 # ROS dependencies
 sudo apt-get install ros-indigo-desktop-full -y
 sudo apt-get install ros-indigo-audio-common -y
