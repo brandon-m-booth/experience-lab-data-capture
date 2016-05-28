dc5f60d797c11a99053af910dabe3affc5211402
Brandon Booth <bbooth@usc.edu>
2016-05-27 19:00:12 -0700

    Added bag tool to extract topics to common formats
    Only video is supported currently (image/compressed->mp4)
    
    
    Former-commit-id: 499f056198e76289f3c3b6991bcda8abb0b8728a

---
+++ ros/src/bag_tools/scripts/bag_topic_extractor.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import subprocess
+import platform
+import os
+import sys
+import pdb
+import rospy
+import rosbag
+from std_msgs.msg import Float32
+
+def doExtractCompressedFrames(bag, bag_path, topic, out_folder):
+   frames_dir = '/tmp/temp_frames'
+   if os.path.isdir(frames_dir):
+      os.system('rm -rf '+frames_dir)
+   os.mkdir(frames_dir)
+
+   frame_count = 0
+   for bag_topic, msg, t in bag.read_messages():
+      if bag_topic == topic:
+         image_format = msg.format
+         if 'jpeg' in image_format:
+            image_format = 'jpg'
+
+         out_frame = open(frames_dir+'/frame%04d.'%(frame_count)+image_format, 'wb')
+         out_frame.write(bytearray(msg.data))
+         out_frame.close()
+         frame_count = frame_count + 1
+
+   frame_rate = round(frame_count/(bag.get_end_time()-bag.get_start_time()))
+
+   # Generate a video from the frames
+   bag_name = os.path.basename(bag_path)
+   topic_file_suffix = topic.replace('/','_')
+   out_file = out_folder+'/'+bag_name[:-4]+topic_file_suffix+'.mp4'
+   if os.path.isfile(out_file):
+      os.remove(out_file)
+   os.system('cd '+frames_dir+'; ffmpeg -framerate '+str(frame_rate)+' -i frame%04d.jpg -c:v libx264 -r '+str(frame_rate)+' -pix_fmt yuv420p '+out_file)
+   return
+
+def doExtractTopic(bag_path, topic, out_folder):
+   bag = rosbag.Bag(bag_path, 'r')
+   type_topics = bag.get_type_and_topic_info()
+   topics = type_topics[1].keys()
+   types = []
+   for i in range(len(type_topics[1].values())):
+      types.append(type_topics[1].values()[i][0])
+
+   topic_indices = []
+   try:
+      for i in range(len(topics)):
+         bag_topic = topics[i]
+         if bag_topic.startswith(topic):
+            topic_indices.append(i)
+   except ValueError:
+      print 'Topic not found in the source bag.  Please check the name and try again.'
+      return
+
+   msg_types = []
+   for topic_index in topic_indices:
+      msg_types.append(types[topic_index])
+
+   for i in range(len(topic_indices)):
+      bag_topic = topics[topic_indices[i]]
+      msg_type = msg_types[i]
+      if msg_type == 'sensor_msgs/CompressedImage':
+         doExtractCompressedFrames(bag, bag_path, bag_topic, out_folder)
+
+   bag.close()
+   return
+   
+                     
+if __name__=='__main__':
+   rospy.init_node('bagTopicExtractor')
+
+   if len(sys.argv) < 4:
+      print 'Please provide the following command line args:\n bag_topic_extractor.py [bag_name] [topic] [out_folder]'
+   bag_name = sys.argv[1]
+   topic = sys.argv[2]
+   out_folder = sys.argv[3]
+
+   doExtractTopic(bag_name, topic, out_folder)
