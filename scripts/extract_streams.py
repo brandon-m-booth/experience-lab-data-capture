621bdfc0a122f0022895164cdb4139fa3c853971
Brandon Booth <brandon.m.booth@gmail.com>
2016-07-01 00:01:33 -0700

    Added stream extraction support for annotations
    Former-commit-id: c92365fe140cc987b6bec119cc2085ab44d076f1

--- scripts/extract_streams.py
+++ scripts/extract_streams.py
@@ -6,21 +6,25 @@ import pdb
 import platform
 
 def doExtractStreams():
-   #topics = ['/audio', '/eye_position', '/kinect2/rgb_rect/image', '/game_video/rgb/image', '/playerInput/cursorPosition', '/playerInput/keyEvents', '/player_app_activity', '/player_eeg', '/player_video/overhead/camera/image_mono']
-   topics = ['/audio', '/eye_position', '/playerInput/cursorPosition', '/playerInput/keyEvents', '/player_app_activity', '/player_eeg']
+   topics = ['/audio', '/eye_position', '/kinect2/rgb_rect/image', '/game_video/rgb/image', '/playerInput/cursorPosition', '/playerInput/keyEvents', '/player_app_activity', '/player_eeg', '/player_video/overhead/camera/image_mono']
+   for annotator_id in range(15):
+      for session in ['lecture', 'student']:
+         for stream in ['engagement', 'isTakingNotes']:
+            topics.append('annotation/'+session+'/annotator'+str(annotator_id)+'/'+stream)
    filter_re_prog = re.compile('.*[main|main2|b1|b2]')
    bag_file_paths = []
 
    # Get list of all subject file paths in all subject subfolders
-   base_path = '/home/coder/Desktop/engagement_data/'
-   out_dir_base = '/home/coder/Desktop/engagement_data_streams/'
-   subject_folders = os.listdir(base_path)
-   for subject_folder in subject_folders:
-      subject_files = os.listdir(base_path+subject_folder)
-      for subject_file in subject_files:
-         re_result = filter_re_prog.match(subject_file)
-         if re_result is not None:
-            bag_file_paths.append(base_path+subject_folder+'/'+subject_file)
+   base_path = '/media/brandon/SailData/MOOC_Engagement_Pilot_Study/stitched_bags/'
+   out_dir_base = '/USC/2016_Engagement_Pilot/data/'
+   for subject_file in os.listdir(base_path):
+      bag_file_paths.append(base_path+'/'+subject_file)
+   #for subject_folder in subject_folders:
+   #   subject_files = os.listdir(base_path+subject_folder)
+   #   for subject_file in subject_files:
+   #      re_result = filter_re_prog.match(subject_file)
+   #      if re_result is not None:
+   #         bag_file_paths.append(base_path+subject_folder+'/'+subject_file)
 
    print 'Going to process these bag files: '
    print bag_file_paths
@@ -30,6 +34,10 @@ def doExtractStreams():
       for topic in topics:
          # Put each topic in a subfolder
          out_sub_dir = topic
+
+         if topic.find('annotation') != -1:
+            out_sub_dir = 'annotation'
+
          if out_sub_dir.startswith('/'):
             out_sub_dir = out_sub_dir[1:]
          out_sub_dir = out_sub_dir.replace('/','_')
@@ -37,6 +45,7 @@ def doExtractStreams():
             os.mkdir(out_dir_base+out_sub_dir)
 
          # Extract!
+         print 'rosrun bag_tools bag_topic_extractor.py '+bag_file_path+' '+topic+' '+out_dir_base+out_sub_dir
          os.system('rosrun bag_tools bag_topic_extractor.py '+bag_file_path+' '+topic+' '+out_dir_base+out_sub_dir)
 
    return
