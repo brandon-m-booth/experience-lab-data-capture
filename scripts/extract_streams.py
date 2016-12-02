a9aec12065a4127a51c9df3026b52d8111fefef8
Brandon Booth <brandon.m.booth@gmail.com>
2016-12-01 20:49:05 -0800

    Fixed accidental block comment
    Former-commit-id: f1554e0369f2d620ec07466fd78052f6cdb0c6da

--- scripts/extract_streams.py
+++ scripts/extract_streams.py
@@ -17,14 +17,13 @@ def doExtractStreams():
    # Get list of all subject file paths in all subject subfolders
    base_path = '/media/brandon/SailData/MOOC_Engagement_Pilot_Study/stitched_bags/'
    out_dir_base = '/USC/2016_Engagement_Pilot/data/'
-   for subject_file in os.listdir(base_path):
-      bag_file_paths.append(base_path+'/'+subject_file)
-   #for subject_folder in subject_folders:
-   #   subject_files = os.listdir(base_path+subject_folder)
-   #   for subject_file in subject_files:
-   #      re_result = filter_re_prog.match(subject_file)
-   #      if re_result is not None:
-   #         bag_file_paths.append(base_path+subject_folder+'/'+subject_file)
+   subject_folders = [base_path]
+   for subject_folder in subject_folders:
+      subject_files = os.listdir(subject_folder)
+      for subject_file in subject_files:
+         re_result = filter_re_prog.match(subject_file)
+         if re_result is not None:
+            bag_file_paths.append(os.path.join(subject_folder,subject_file))
 
    print 'Going to process these bag files: '
    print bag_file_paths
