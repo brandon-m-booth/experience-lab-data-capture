b3015ff70de8c5334d0febf71a644e7105a93363
Brandon Booth <brandon.m.booth@gmail.com>
2016-12-02 20:46:23 -0800

    Improved script extracting data streams from bags
    Former-commit-id: 4a23a39303cce3613b24b01820ac314fa7cd47c6

--- scripts/extract_streams.py
+++ scripts/extract_streams.py
@@ -3,21 +3,21 @@
 import os
 import re
 import pdb
+import sys
 import platform
 
-def doExtractStreams():
-   topics = ['/audio', '/eye_position', '/kinect2/rgb_rect/image', '/game_video/rgb/image', '/playerInput/cursorPosition', '/playerInput/keyEvents', '/player_app_activity', '/player_eeg', '/player_video/overhead/camera/image_mono']
-   for annotator_id in range(15):
-      for session in ['lecture', 'student']:
-         for stream in ['engagement', 'isTakingNotes']:
-            topics.append('annotation/'+session+'/annotator'+str(annotator_id)+'/'+stream)
+def doExtractStreams(bag_path, out_path, topics=None):
+   if not topics:
+      topics = ['/audio', '/eye_position', '/kinect2/rgb_rect/image', '/game_video/rgb/image', '/playerInput/cursorPosition', '/playerInput/keyEvents', '/player_app_activity', '/player_eeg', '/player_video/overhead/camera/image_mono']
+      for annotator_id in range(30):
+         for session in ['lecture', 'student']:
+            for stream in ['engagement', 'isTakingNotes']:
+               topics.append('annotation/'+session+'/annotator'+str(annotator_id)+'/'+stream)
    filter_re_prog = re.compile('.*[main|main2|b1|b2]')
    bag_file_paths = []
 
    # Get list of all subject file paths in all subject subfolders
-   base_path = '/media/brandon/SailData/MOOC_Engagement_Pilot_Study/stitched_bags/'
-   out_dir_base = '/USC/2016_Engagement_Pilot/data/'
-   subject_folders = [base_path]
+   subject_folders = [bag_path]
    for subject_folder in subject_folders:
       subject_files = os.listdir(subject_folder)
       for subject_file in subject_files:
@@ -40,14 +40,24 @@ def doExtractStreams():
          if out_sub_dir.startswith('/'):
             out_sub_dir = out_sub_dir[1:]
          out_sub_dir = out_sub_dir.replace('/','_')
-         if not os.path.isdir(out_dir_base+out_sub_dir):
-            os.mkdir(out_dir_base+out_sub_dir)
+         out_dir = os.path.join(out_path, out_sub_dir)
+         if not os.path.isdir(out_dir):
+            os.makedirs(out_dir)
 
          # Extract!
-         print 'rosrun bag_tools bag_topic_extractor.py '+bag_file_path+' '+topic+' '+out_dir_base+out_sub_dir
-         os.system('rosrun bag_tools bag_topic_extractor.py '+bag_file_path+' '+topic+' '+out_dir_base+out_sub_dir)
+         print 'rosrun bag_tools bag_topic_extractor.py '+bag_file_path+' '+topic+' '+out_dir
+         os.system('rosrun bag_tools bag_topic_extractor.py '+bag_file_path+' '+topic+' '+out_dir)
 
    return
 
 if __name__=='__main__':
-   doExtractStreams()
+   if len(sys.argv) > 2:
+      bag_path = sys.argv[1]
+      out_path = sys.argv[2]
+      if len(sys.argv) > 3:
+         topics = sys.argv[3].split(',')
+      else:
+         topics = None
+      doExtractStreams(bag_path, out_path, topics)
+   else:
+      print 'Please provide the following command line arguments:\n1) Path to folder containing bag files\n2) Output directory for extracted stream data\n3) (OPTIONAL) Comma-separated list of topics to extract'
